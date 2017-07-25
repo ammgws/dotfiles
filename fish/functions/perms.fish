f45f751862225bb601d2bbbe161a2ef8d345bde5
Jason <jason@kayoway.com>
2017-07-25 23:58:02 +0900

    Emulate ls -l output (however date format is different)

--- fish/functions/perms.fish
+++ fish/functions/perms.fish
@@ -1,4 +1,4 @@
 function perms --description 'return file/folder permissions in octal form'
-    stat -c '%A %a %h %U %G %s %Y %n' $argv | sed 
-"s/^.*\([0-9]\{13\}\).*/date -d @\1 +'%b %e %H:%M'/" 
+    #stat -c '%A %a %h %U %G %s %Y %n' $argv | sed -r 's|.*\([0-9]\{10\}\).*|(date +"%b %e %H:%M" -d @\1)|eg'
+    stat -c "%A/%a/%h/%U/%G/%s/%y/%n" $argv | column -t -s'/'
 end
