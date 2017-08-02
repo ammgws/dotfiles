67f6d0c9c2d2aeebc912683a35f1401705f137a9
Jason <jason@kayoway.com>
2017-08-03 00:35:50 +0900

    fix end modes for underline and standout

--- fish/functions/man.fish
+++ fish/functions/man.fish
@@ -8,11 +8,10 @@ function man --description "Colourisation and other customisations for manpages"
     # https://www.gnu.org/software/termutils/manual/termcap-1.3/html_chapter/termcap_4.html#SEC33
     # http://linux.101hacks.com/ps1-examples/prompt-color-using-tput/
     set -lx LESS_TERMCAP_md (tput bold; tput setaf 39)  # enter bold mode, blue text
-    set -lx LESS_TERMCAP_mb (tput blink)  # enter blink mode
-    set -lx LESS_TERMCAP_us (tput smul; tput bold; tput setaf 40)  # enter underline mode, green text
-    set -lx LESS_TERMCAP_ue (tput rmul)  # leave underline mode
-    set -lx LESS_TERMCAP_so (tput smso; tput setab 91)  # enter standout (reverse video) mode, purple bg
-    set -lx LESS_TERMCAP_se (tput rmso)  # leave standout (reverse video) mode
+    set -lx LESS_TERMCAP_us (tput smul; tput setaf 40)  # enter underline mode, green text
+    set -lx LESS_TERMCAP_ue (tput rmul; tput sgr0)  # leave underline mode
+    set -lx LESS_TERMCAP_so (tput smso; tput setab 127)  # enter standout/reverse mode, purple bg
+    set -lx LESS_TERMCAP_se (tput rmso; tput sgr0)  # leave standout/reverse mode
     set -lx LESS_TERMCAP_mh (tput dim)  # enter half-bright mode
     set -lx LESS_TERMCAP_me (tput sgr0)  # turns off all modes
 
