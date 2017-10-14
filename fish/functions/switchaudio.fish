de6a9db761ec1e59c56c22d14032a48aeba24dfb
Jason <jason@kayoway.com>
2017-10-14 18:24:13 +0900

    Change default-sink, code cleanup

--- fish/functions/switchaudio.fish
+++ fish/functions/switchaudio.fish
@@ -2,14 +2,22 @@ function switchaudio --description 'Switch between audio outputs (assuming there
     set -l sinks (pactl list short sinks)
     for sink in $sinks
        set sink_info (string split \t $sink)
-       if test RUNNING = $sink_info[5]
-           set active_sink $sink_info[1]
-       else if test SUSPENDED = $sink_info[5]
-           set inactive_sink $sink_info[1]
-       else if test IDLE = $sink_info[5]
-           set inactive_sink $sink_info[1]
+       set sink_id $sink_info[1]
+       set sink_state $sink_info[5]
+
+       switch $sink_state
+           case RUNNING
+               set active_sink $sink_id
+           case SUSPENDED
+               set inactive_sink $sink_id
+           case IDLE
+               set inactive_sink $sink_id
        end
     end
+
+    # If the output we switch to is not set as default then the
+    # system volume slider controls will not affect it.
+    pactl set-default-sink $inactive_sink
 
     set -l inputs (pactl list short sink-inputs)
     for input in $inputs
