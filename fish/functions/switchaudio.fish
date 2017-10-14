d1db406328b53d5e2bbed13a70238e970048157c
Jason <jason@kayoway.com>
2017-10-14 18:04:51 +0900

    Add sink move logic (code is now working)

--- fish/functions/switchaudio.fish
+++ fish/functions/switchaudio.fish
@@ -1,4 +1,4 @@
-function switchaudio --description 'Switch between audio outputs'
+function switchaudio --description 'Switch between audio outputs (assuming there are only 2)'
     set -l sinks (pactl list short sinks)
     for sink in $sinks
        set sink_info (string split \t $sink)
@@ -6,11 +6,14 @@ function switchaudio --description 'Switch between audio outputs'
            set active_sink $sink_info[1]
        else if test SUSPENDED = $sink_info[5]
            set inactive_sink $sink_info[1]
+       else if test IDLE = $sink_info[5]
+           set inactive_sink $sink_info[1]
        end
     end
 
-    echo $active_sink
-    echo $inactive_sink
-
-    set -l inputs (string split \t (pactl list short sink-inputs))
+    set -l inputs (pactl list short sink-inputs)
+    for input in $inputs
+        set input_id (string split \t $input)[1]
+        pactl move-sink-input $input_id $inactive_sink
+    end
 end
