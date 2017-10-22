80b0b62f6b778fe383807a0b8c0b18221c6f80a6
Jason <jason@kayoway.com>
2017-10-23 01:03:23 +0900

    Add toast notification on output change

--- fish/functions/switchaudio.fish
+++ fish/functions/switchaudio.fish
@@ -1,9 +1,33 @@
 function switchaudio --description 'Switch between audio outputs (assuming there are only 2)'
+    # TODO: handle case when nothing is playing (both sinks are SUSPENDED)
+    function set_output_name
+        set sink_raw_name $argv
+        if string match --quiet '*Pebbles*' $sink_raw_name
+            set --global sink_name "speakers"
+        else if string match --quiet '*pci*' $sink_raw_name
+            set --global sink_name "headphones"
+        else
+            set --global sink_name $sink_raw_name
+        end
+    end
+
+    # set current_default (pacmd stat | awk -F": " '/^Default sink name: /{print $2}')
+    set current_default_raw (pacmd stat | string match "Default sink name: *" | string replace "Default sink name: " "")
+
     set sinks (pactl list short sinks)
     for sink in $sinks
        set sink_info (string split \t $sink)
        set sink_id $sink_info[1]
+       set_output_name $sink_info[2]
        set sink_state $sink_info[5]
+
+       if test $sink_info[2] = $current_default_raw
+           set current_default_id $sink_id
+           set current_default_name $sink_name
+       else
+           set switch_to_default_id $sink_id
+           set switch_to_default_name $sink_name
+       end
 
        switch $sink_state
            case RUNNING
@@ -24,4 +48,6 @@ function switchaudio --description 'Switch between audio outputs (assuming there
         set input_id (string split \t $input)[1]
         pactl move-sink-input $input_id $inactive_sink
     end
+
+    notify-send (string join " " "Switched to" $switch_to_default_name) --icon=audio-volume-high
 end
