5a17f83c43ff226f368bda53b18999af4acf8992
Jason <jason@kayoway.com>
2018-02-28 22:58:32 +0900

    Use long options, move helper functions from fish_greeting

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,17 +1,18 @@
 function fish_prompt
   # Start ohmyfish-batman theme default prompt
   test $status -ne 0;
-    and set -l colors 600 900 c00
-    or set -l colors 333 666 aaa
+    and set --local colors 600 900 c00
+    or set --local colors 333 666 aaa
 
-  set -l pwd (prompt_pwd)
-  set -l base (basename "$pwd")
+  set --local pwd (prompt_pwd)
+  set --local base (basename "$pwd")
 
-  set -l expr "s|~|"(fst)"^^"(off)"|g; \
-               s|/|"(snd)"/"(off)"|g;  \
-               s|"$base"|"(fst)$base(off)" |g"
 
-  echo -n (echo "$pwd" | sed -e $expr)(off)
+  set --local expr "s|~|"(fst)"^^"(off)"|g; \
+                   s|/|"(snd)"/"(off)"|g;  \
+                   s|"$base"|"(fst)$base(off)" |g"
+
+  echo -n (echo "$pwd" | sed --expression $expr)(off)
 
   for color in $colors
     echo -n (set_color $color)">"
@@ -20,7 +21,8 @@ function fish_prompt
   echo -n " "
   # End ohmyfish-batman theme default prompt
 
-  if set -q VIRTUAL_ENV
-    echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+  if set --query VIRTUAL_ENV
+    echo -n -s (set_color --background blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+  end
   end
 end
