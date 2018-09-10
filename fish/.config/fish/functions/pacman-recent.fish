14ead73d44fe9d12d5e4e50f2e4719063535faf8
Jason <jason@kayoway.com>
2018-09-11 01:03:10 +0900

    First commit

---
+++ fish/.config/fish/functions/pacman-recent.fish
@@ -0,0 +1,3 @@
+function pacman-recent --description="Show last 25 upgraded/installed packages"
+  command tac /var/log/pacman.log | head -n 1000 | grep --ignore-case --extended-regexp 'installed|upgraded' | tail --lines=25
+end
