1d246ac45c31c718ae3f3f9057771d2e4f6010e2
Jason <jason@kayoway.com>
2018-10-23 20:52:25 +0900

    Update AUR helper function

--- fish/.config/fish/functions/upgrade-aur.fish
+++ fish/.config/fish/functions/upgrade-aur.fish
@@ -1,6 +1,6 @@
 function upgrade-aur
   set_color --bold brmagenta
-  echo "Running command: trizen --sync --refresh --sysupgrade --devel"
+  echo "Running command: pikaur --sync --refresh --sysupgrade --devel"
   set_color normal
-  command pikaur --sync --refresh --sysupgrade
+  command pikaur --sync --refresh --sysupgrade --devel
 end
