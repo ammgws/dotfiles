cedc91b33a8a7b66cc766dcc922c126d364a429a
Jason <jason@kayoway.com>
2019-09-04 23:57:44 +0900

    Improve SSH user experience, others

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,7 +6,7 @@ eval (python -m virtualfish auto_activation)
 
 set --export XKB_DEFAULT_LAYOUT us
 
-set --export TERMINAL urxvt
+set --export TERMINAL kitty
 set --export BROWSER /usr/bin/google-chrome-stable
 
 # Encourage programs to use Wayland
@@ -14,20 +14,23 @@ set --export BROWSER /usr/bin/google-chrome-stable
 set --export CLUTTER_BACKEND wayland
 # only for GTK3+. Breaks ibus widget and google-chrome atm.
 #set --export GDK_BACKEND wayland
+set --export MOZ_ENABLE_WAYLAND 1
+set --export MOZ_WEBRENDER 1
 set --export ECORE_EVAS_ENGINE wayland_egl
 set --export ELM_ENGINE wayland_egl
 set --export QT_QPA_PLATFORM wayland-egl
 set --export QT_WAYLAND_DISABLE_WINDOWDECORATION 1
 set --export SDL_VIDEODRIVER wayland
 set --export _JAVA_AWT_WM_NONREPARENTING 1
-set --export KITTY_ENABLE_WAYLAND 1
+
+# not used anymore
+#set --export KITTY_ENABLE_WAYLAND 1
 
 # Encourage use of XDG dirs
 set --export XDG_CACHE_HOME ~/.cache
 set --export XDG_CONFIG_HOME ~/.config
 set --export XDG_DATA_HOME ~/.local/share
 set --export XDG_RUNTIME_DIR /run/user/(id -u)
-
 set --export DVDCSS_CACHE $XDG_DATA_HOME/dvdcss
 set --export GNUPGHOME $XDG_CONFIG_HOME/gnupg
 set --export ICEAUTHORITY $XDG_RUNTIME_DIR/ICEauthority
@@ -39,12 +42,32 @@ set --export XAUTHORITY $XDG_RUNTIME_DIR/Xauthority
 set --export XINITRC $XDG_CONFIG_HOME/X11/xinitrc
 
 # For Japanese IME support
-set --export IBUS_SOCK $XDG_RUNTIME_DIR/ibus.socket
 set --export GTK_IM_MODULE ibus
 set --export QT_IM_MODULE ibus
 set --export XMODIFIERS @im=ibus
 set --export DefaultIMModule ibus
 
+# User experience improvements over SSH
+if set --query SSH_CLIENT
+  if ! set --query DBUS_SESSION_BUS_ADDRESS
+    set --local dbus_session_file $HOME/.dbus/session-bus/(cat /var/lib/dbus/machine-id)-0
+    if test -e $dbus_session_file
+      set --export DBUS_SESSION_BUS_ADDRESS (grep "^DBUS_SESSION_BUS_ADDRESS=" $dbus_session_file)
+    end
+  end
+
+  if ! set --query SWAYSOCK
+    set --export SWAYSOCK (ls /run/user/1000/sway-ipc.* | head --lines 1)
+  end
+end
+
+# Abbreviations (text expansion)
+abbr --add gp 'git push'
+#abbr --add mv 'smv'
+
+# Other
+set --export FZF_DEFAULT_COMMAND "fd --type f"
+
 # Used in my fish functions
 set --export AMMCON_URL https://ammcon:port
-set --export SCREENSHOT_DIR /path/to/screenshots
+set --export SCREENSHOT_DIR $HOME/Dropbox/screenshots
