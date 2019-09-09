7787475bde7495b6a3b98ec16a8c48d897b0f591
Jason <jason@kayoway.com>
2019-09-09 21:17:02 +0900

    Update output format for status bar

--- fish/.config/fish/functions/check_kernel.fish
+++ fish/.config/fish/functions/check_kernel.fish
@@ -1,9 +1,11 @@
 function check_kernel --description='Output message if installed and running kernels are different.'
+  #                                                     e.g. linux 4.18.5.arch1-1
   set installed (string match --regex '\d\d?.\d\d?.\d\d?' (pacman --query linux))
-  #                                                        linux 4.18.5.arch1-1
+  #                                                     e.g. 4.18.4-arch1-1-ARCH
   set running (string match --regex '\d\d?.\d\d?.\d\d?' (uname --kernel-release))
-  #                                                      4.18.4-arch1-1-ARCH
   if test ! $running = $installed
-    printf 'Kernel out of date!'
+    printf "🐧😞"
+  else
+    printf "🐧☺️"
   end
 end
