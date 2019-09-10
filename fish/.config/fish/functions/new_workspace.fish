97321a1cc872628bf907b285fc51d4c5b15494ed
Jason <jason@kayoway.com>
2019-09-10 13:53:28 +0900

    (wip) update ws function

--- fish/.config/fish/functions/new_workspace.fish
+++ fish/.config/fish/functions/new_workspace.fish
@@ -1,9 +1,17 @@
 function new_workspace
 
   # append will be in fish 3.0
-  set $workspaces ""
+  set used_workspaces ""
   for workspace in (swaymsg --raw --type get_workspaces | jq --raw-output '.[].name')
-    set --append $workspaces $workspace
+    set --append used_workspaces $workspace
   end
+  echo $used_workspaces
 
+  set free_workspaces (seq 1 50)
+  for ws in $used_workspaces
+    if set --local index (contains --index $ws $free_workspaces)
+      set --erase free_workspaces[$index]
+    end
+  end
+  echo $free_workspaces
 end
