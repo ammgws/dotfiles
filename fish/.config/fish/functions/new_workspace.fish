dfe8dc0fa3dc72837e28f2a9651038bbad560049
Jason <jason@kayoway.com>
2019-09-11 22:21:54 +0900

    Update with working code

--- fish/.config/fish/functions/new_workspace.fish
+++ fish/.config/fish/functions/new_workspace.fish
@@ -1,11 +1,29 @@
-function new_workspace
+function new_workspace --description="Create new workspace on the current monitor."
+  function print_help
+    echo "Usage: new_workspace [options]"
+    echo "Options:"
+    echo (set_color green)"--move-focused"(set_color $fish_color_normal)": Move the currently focused container to the new workspace as well."
+  end
 
-  # append will be in fish 3.0
+  # default values (that can be changed via args)
+  set MOVE_FOCUSED 0
+
+  argparse --name new_workspace 'h/help' 'm/move-focused' -- $argv
+  or return 1  #error
+
+  if set -lq _flag_help
+     print help
+     return
+  end
+
+  if set -lq _flag_movefocused
+      set MOVE_FOCUSED 1
+  end
+
   set used_workspaces ""
   for workspace in (swaymsg --raw --type get_workspaces | jq --raw-output '.[].name')
     set --append used_workspaces $workspace
   end
-  echo $used_workspaces
 
   set free_workspaces (seq 1 50)
   for ws in $used_workspaces
@@ -13,5 +31,13 @@ function new_workspace
       set --erase free_workspaces[$index]
     end
   end
-  echo $free_workspaces
+
+  set new_workspace $free_workspaces[1]
+  if test $MOVE_FOCUSED = 1
+    set focused_container (swaymsg -t get_tree | jq '.. | objects | select(.focused == true) | .id')
+    # or swaymsg -t get_tree | jq --raw-output '.. | (.nodes? // empty)[] | select(.focused==true) | .id'
+    swaymsg [con_id="$focused_container"] move workspace $new_workspace
+  else
+    swaymsg workspace $new_workspace
+  end
 end
