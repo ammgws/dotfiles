71952c356380cb1eba8c8ba8cf8a62716ea709b1
Jason <jason@kayoway.com>
2019-09-11 22:36:59 +0900

    Fix indentation

--- fish/.config/fish/functions/check_xclients.fish
+++ fish/.config/fish/functions/check_xclients.fish
@@ -1,31 +1,31 @@
 function check_xclients --description="Return number of current X clients"
   argparse --name check_xclients 'h/help' 'b/barmode' -- $argv
-   or return 1  # error
+  or return 1  # error
 
-   function print_help
-     echo "Usage: check_xclients [options]"
-     echo "Options:"
-     echo (set_color green)"-b"(set_color $fish_color_normal)": Output text for i3status-rs bar"
-   end
+  function print_help
+    echo "Usage: check_xclients [options]"
+    echo "Options:"
+    echo (set_color green)"-b"(set_color $fish_color_normal)": Output text for i3status-rs bar"
+  end
 
-   if set -lq _flag_help
-     print help
-     return
-   end
+  if set -lq _flag_help
+    print help
+    return
+  end
 
-   set MODE std
-   if set -lq _flag_barmode
-     set MODE bar
-   end
+  set MODE std
+  if set -lq _flag_barmode
+    set MODE bar
+  end
 
-   set val (xlsclients | wc --lines)
-   if test $MODE = bar
-     if test val = 0
-       printf "<span color='green'>X0</span>"
-     else
-       printf "<span color='red'>X$val</span>"
-     end
-   else
-     echo $val
-   end
+  set val (xlsclients | wc --lines)
+  if test $MODE = bar
+    if test val = 0
+      printf "<span color='green'>X0</span>"
+    else
+      printf "<span color='red'>X$val</span>"
+    end
+  else
+    echo $val
+  end
 end
