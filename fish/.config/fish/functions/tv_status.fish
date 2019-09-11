8fc6cfe3ac4de3b3b8e7be2f3b57dcadc5ba4842
Jason <jason@kayoway.com>
2019-09-11 22:36:48 +0900

    Simplify jq parsing logic

--- fish/.config/fish/functions/tv_status.fish
+++ fish/.config/fish/functions/tv_status.fish
@@ -1,13 +1,31 @@
-function tv_status
-  set -l current_outputs (swaymsg --raw --type get_outputs | jq --raw-output '.[] | [.model, .active] | @csv')
-  for output in $current_outputs
-    set -l data (string split ',' $output)
-    set -l name (string unescape $data[1])
-    set -l active $data[2]
-    if test $name = "LG TV" -a $active = "true"
+function tv_status --description="Return active status of the connected TV to my desktop."
+  argparse --name check_xclients 'h/help' 'b/barmode' -- $argv
+  or return 1  # error
+
+  function print_help
+    echo "Usage: tv_status [options]"
+    echo "Options:"
+    echo (set_color green)"-b"(set_color $fish_color_normal)": Output text for i3status-rs bar"
+  end
+
+  if set -lq _flag_help
+    print help
+    return
+  end
+
+  set MODE std
+  if set -lq _flag_barmode
+    set MODE bar
+  end
+
+  set --local tv_status (swaymsg -t get_outputs | jq --raw-output '.. | objects | select(.model == "LG TV") | .active')
+  if test $MODE = bar
+    if test $tv_status = "true"
       printf "<span color='green'>📺ON</span>"
-    else if test $name = "LG TV" -a $active = "false"
+    else if test $tv_status = "false"
       printf "<span color='green'>📺OFF</span>"
     end
+  else
+    echo $tv_status
   end
 end
