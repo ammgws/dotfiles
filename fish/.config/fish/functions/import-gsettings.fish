59c9f68b05e08c6a9aae31114fa63c754ff84d39
Jason <jason@kayoway.com>
2019-09-12 23:51:30 +0900

    Use own gsettings import function
    See https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland

---
+++ fish/.config/fish/functions/import-gsettings.fish
@@ -0,0 +1,16 @@
+function import-gsettings
+  function gset --argument-names key val
+    gsettings set org.gnome.desktop.interface $key $val
+  end
+
+  while read --local --array line
+    set config (string match --regex "(gtk-[\w|-]+)=(.+)" $line)
+    if string match --quiet $config[2] gtk-theme-name
+      gset gtk-theme $config[3]
+    else if string match --quiet $config[2] gtk-icon-theme
+      gset icon-theme $config[3]
+    else if string match --quiet $config[2] gtk-cursor-theme
+      gset cursor-theme $config[3]
+    end
+  end < ~/.config/gtk-3.0/settings.ini
+end
