2694f85c8ebc734cadd24a18c1f8a2223edcf17d
Jason <jason@kayoway.com>
2019-09-13 00:06:35 +0900

    Add TODO

--- fish/.config/fish/functions/import-gsettings.fish
+++ fish/.config/fish/functions/import-gsettings.fish
@@ -11,6 +11,9 @@ function import-gsettings
       gset icon-theme $config[3]
     else if string match --quiet $config[2] gtk-cursor-theme
       gset cursor-theme $config[3]
+    #TODO: figure out why this is not capturing the whole string 'Roboto 10'
+    #else if string match --quiet $config[2] gtk-font-name
+    #  gset font-name $config[3]
     end
   end < ~/.config/gtk-3.0/settings.ini
 end
