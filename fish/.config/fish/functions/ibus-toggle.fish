93b79c02bc8583949a5b836fbab47b3c11cd9e5d
Jason <jason@kayoway.com>
2019-09-13 12:28:27 +0900

    Detect errors

--- fish/.config/fish/functions/ibus-toggle.fish
+++ fish/.config/fish/functions/ibus-toggle.fish
@@ -1,4 +1,3 @@
-#TODO: check if command succeeded
 function ibus-toggle --description="Switches between IBus input methods and displays notification."
     set engine (ibus engine)
     if test $engine = "mozc-jp"
@@ -8,6 +7,10 @@ function ibus-toggle --description="Switches between IBus input methods and disp
         ibus engine mozc-jp
         set msg "Japanese input (mozc)"
     end
-
-    notify-send (string join " " "Switched to" $msg) --icon=keyboard --expire-time=1000
+    if test $status -eq 0
+      notify-send (string join " " "Switched to" $msg) --icon=keyboard --expire-time=1000
+    else
+      notify-send "Failed to switch ibus engine" --icon=keyboard --expire-time=1000
+      return 1
+    end
 end
