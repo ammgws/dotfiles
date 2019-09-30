e9030d7ec45829bf3a78ed1651475be6d93f84f4
Jason <jason@kayoway.com>
2019-09-30 22:05:20 +0900

    Move ytdl to abbreviations

--- fish/.config/fish/abbreviations.fish
+++ fish/.config/fish/abbreviations.fish
@@ -4,3 +4,4 @@ abbr --add gst 'git status'  # gs taken by Ghostscript
 abbr --add uparch 'sudo pacman --sync --refresh --sysupgrade'
 abbr --add upaur 'pikaur --sync --refresh --sysupgrade --devel --needed --noconfirm'
 abbr --add send2phone 'kdeconnect-cli --device (kdeconnect-cli --list-available --id-only) --share $PWD/(fzf)'
+abbr --add ytdl 'youtube-dl'
