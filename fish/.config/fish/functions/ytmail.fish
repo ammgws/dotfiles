99070a06728c35e80bd17de380b05c4eb287a158
Jason <jason@kayoway.com>
2019-09-30 21:18:24 +0900

    Improve error handling, add colour

--- fish/.config/fish/functions/ytmail.fish
+++ fish/.config/fish/functions/ytmail.fish
@@ -1,10 +1,4 @@
 function ytmail --description 'Download and email youtube videos. Useful for work since youtube is blocked.'
-    # define colours to use when printing messages
-    set cRed (set_color red)
-    set cGrn (set_color green)
-    set cBlu (set_color blue)
-    set cRst (set_color $fish_color_normal)
-
     function print_help
         echo "Usage: ytmail [options] URL RECIPIENT"
         echo "Options:"
@@ -15,7 +9,7 @@ function ytmail --description 'Download and email youtube videos. Useful for wor
     set DEPENDENCIES $DEPENDENCIES youtube-dl gmailer_oauth
     for dependency in $DEPENDENCIES
         if not type -q $dependency
-            echo $cRed"You must have $dependency installed."$cRst
+            echo (set_color red)"You must have $dependency installed."(set_color $fish_color_normal)
             return 1
         end
     end
@@ -25,21 +19,20 @@ function ytmail --description 'Download and email youtube videos. Useful for wor
     set RANDOMIZE 0
     set SIZE "9M"
 
-    argparse --name screenshot 'h/help' 'd-debug' 's-splitsize' -- $argv
+    argparse --name ytmail 'h/help' 'd-debug' 's-splitsize=' -- $argv
     or return 1  #error
 
     if set -lq _flag_help
-        print help
+        print_help
         return
     end
 
     if set -lq _flag_splitsize
-        set SIZE _flag_splitsize
+        set SIZE $_flag_splitsize
     end
 
     set URL $argv[1]
     set RECIPIENT $argv[2]
-
 
     set RANDOM_STR (cat /dev/random | tr --delete --complement 'a-zA-Z0-9' | fold --width 16 | head --lines 1)
     set VIDEOID (youtube-dl --get-id $URL)
@@ -47,26 +40,28 @@ function ytmail --description 'Download and email youtube videos. Useful for wor
     set ARCHIVE_FILEPATH $OUTPUT_DIR"/"$VIDEOID"_"$RANDOM_STR".7z"
     set VIDEO_FILEPATH $OUTPUT_DIR"/"(youtube-dl --get-filename --output "%(title)s.%(ext)s" $URL)
 
-    echo "Video file:" $VIDEO_FILEPATH
-    echo "Archive file:" $ARCHIVE_FILEPATH".*"
+    echo (set_color green)"ytmail: Video file: $VIDEO_FILEPATH"(set_color $fish_color_normal)
+    echo (set_color green)"ytmail: Archive file: $ARCHIVE_FILEPATH.*"(set_color $fish_color_normal)
 
     youtube-dl --recode-video "mp4" --output $VIDEO_FILEPATH $URL
 
     if test -e $VIDEO_FILEPATH
-        echo "Found video file, creating archive now..."
+        echo (set_color green)"ytmail: Found video file, creating archive now..."(set_color $fish_color_normal)
         7z a -v$SIZE $ARCHIVE_FILEPATH $VIDEO_FILEPATH >/dev/null
     else
-        echo "Video download failed. Abort."
+        echo (set_color red)"ytmail: Video download failed. Abort."(set_color $fish_color_normal)
         return 1
     end
 
-    if test $status -eq 0
-        echo "Archive found"
-        for file in $ARCHIVE_FILEPATH.*
-            gmailer_oauth --attachment $file $RECIPIENT
-        end
-    else
-        echo "7z stage failed"
+    if test $status -ne 0
+        echo (set_color red)"ytmail: Archiving stage failed. Abort."(set_color $fish_color_normal)
         return 1
+    end
+
+    for file in $ARCHIVE_FILEPATH.*
+      if test -e $file
+          echo (set_color green)"ytmail: Archive found"(set_color $fish_color_normal)
+          gmailer_oauth --attachment $file $RECIPIENT
+      end
     end
 end
