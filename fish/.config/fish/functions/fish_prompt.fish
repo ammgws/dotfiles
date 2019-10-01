8a80c5babe59f8ec13553987d103e13416cd3a76
Jason <jason@kayoway.com>
2019-10-01 12:54:03 +0900

    Fix up prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,17 +1,13 @@
 function fish_prompt
-  # Start ohmyfish-batman theme default prompt
-  test $status -ne 0;
+  set --local code $status
+  test $code -ne 0;
     and set --local colors 600 900 c00
     or set --local colors 333 666 aaa
 
+  fish_prompt_helpers
+
   set --local pwd (prompt_pwd)
   set --local base (basename "$pwd")
-
-  function fst; set_color --bold fa0; end
-  function snd; set_color --bold 36f; end
-  function trd; set_color --bold f06; end
-  function dim; set_color 666; end
-  function off; set_color normal; end
 
   set --local expr "s|~|"(fst)"^^"(off)"|g; \
                    s|/|"(snd)"/"(off)"|g;  \
@@ -22,12 +18,14 @@ function fish_prompt
   for color in $colors
     echo -n (set_color $color)">"
   end
-
-  echo -n " "
-  # End ohmyfish-batman theme default prompt
+  if test $code -ne 0
+    echo -n "("(trd)"$code"(dim)") "(off)
+  else
+    echo -n " "
+  end
 
   if set --query VIRTUAL_ENV
-    echo -n -s (set_color --background blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+    echo -n -s (set_color --background blue white) "[" (basename "$VIRTUAL_ENV") "]" (set_color normal) " "
   end
 
   # could or may also need to check SSH_TTY or SSH_CONNECTION
