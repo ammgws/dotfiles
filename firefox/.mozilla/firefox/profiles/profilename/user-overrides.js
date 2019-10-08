6867ac9ba52927445bf85d8e037952e263e10811
Jason <jason@kayoway.com>
2019-10-08 22:44:59 +0900

    Add user.js for Firefox

---
+++ firefox/.mozilla/firefox/profiles/profilename/user-overrides.js
@@ -0,0 +1,14 @@
+/******
+* Overrides for ghacks-user.js
+*
+* NOTE: - Use `updater.sh` from the repo to update user.js
+*       - Be aware of the caveats mentioned at the below link:
+*         https://github.com/ghacksuserjs/ghacks-user.js/wiki/3.2-Applying-Your-Changes
+*
+******/
+
+user_pref("alerts.showFavicons", true);  // 1032
+user_pref("privacy.clearOnShutdown.cookies", false);  // 2803
+user_pref("privacy.clearOnShutdown.downloads", false);  // 2803
+user_pref("privacy.clearOnShutdown.history", false);  // 2803
+user_pref("privacy.clearOnShutdown.sessions", false);  // 2803
