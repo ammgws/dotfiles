686f4abf0a298c1ac3123e3a8399de27934e5033
Jason <jason@kayoway.com>
2019-11-22 23:10:44 +0900

    Allow from output to be specified, also fix case when output is empty

--- fish/.config/fish/functions/move_all_workspaces.fish
+++ fish/.config/fish/functions/move_all_workspaces.fish
@@ -1,6 +1,30 @@
 function move_all_workspaces --description="Move all workspaces to the currently focused output."
+ function print_help
+    echo "Usage: move_all_workspaces [options]"
+    echo "Options:"
+    echo (set_color green)"--from"(set_color $fish_color_normal)": Specify output name from which workspaces are to be moved (defaults to all)."
+  end
+
+  argparse --name move_all_workspaces 'f/from=' 'h/help' -- $argv
+  or return 1  #error
+
+  if set -lq _flag_help
+    print_help
+    return
+  end
+
+  if set -lq _flag_from
+    set from_output $_flag_from
+  end
+
   set current_output (swaymsg -t get_outputs | jq '.[] | select(.focused==true) | .name')
-  for ws in (swaymsg --raw --type get_workspaces | jq --raw-output '.[].name')
+
+  if set --query from_output
+    set workspaces (swaymsg --raw --type get_workspaces | jq --raw-output ".[] | select(.representation != null and .output == \"$from_output\") | .name")
+  else
+    set workspaces (swaymsg --raw --type get_workspaces | jq --raw-output '.[] | select(.representation != null) | .name')
+  end
+  for ws in $workspaces
     swaymsg [workspace="$ws"] move workspace to output $current_output
   end
 end
