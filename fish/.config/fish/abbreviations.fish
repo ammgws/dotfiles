40273427fc3876140ae115c200003f9e4f3b1143
Jason <jason@kayoway.com>
2019-12-01 08:31:30 +0900

    Update abbrevs

--- fish/.config/fish/abbreviations.fish
+++ fish/.config/fish/abbreviations.fish
@@ -1,6 +1,8 @@
 # Abbreviations (text expansion)
 abbr --add gp 'git push'
+abbr --add gpf 'git push --force'
 abbr --add gst 'git status'  # gs taken by Ghostscript
+abbr --add icat 'kitty +kitten icat'
 abbr --add uparch 'sudo pacman --sync --refresh --sysupgrade; and fish_update_completions'
 abbr --add upaur 'pikaur --sync --refresh --sysupgrade --devel --needed --noconfirm; and fish_update_completions'
 abbr --add send2phone 'kdeconnect-cli --device (kdeconnect-cli --list-available --id-only) --share $PWD/(fzf)'
