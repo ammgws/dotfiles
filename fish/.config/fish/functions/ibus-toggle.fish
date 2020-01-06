3988d099ac5832e31b8107f5f79799cb16512ac8
Jason <jason@kayoway.com>
2020-01-06 16:04:53 +0900

    Quote args to `test`, re-order to fix err logic

--- fish/.config/fish/functions/ibus-toggle.fish
+++ fish/.config/fish/functions/ibus-toggle.fish
@@ -1,16 +1,14 @@
 function ibus-toggle --description="Switches between IBus input methods and displays notification."
     set engine (ibus engine)
-    if test $engine = "mozc-jp"
+    if test $status -ne 0
+        notify-send "Failed to switch ibus engine" --icon=keyboard --expire-time=1000
+        return 1
+    else if test "$engine" = "mozc-jp"
         ibus engine xkb:us::eng
         set msg "English input"
     else
         ibus engine mozc-jp
         set msg "Japanese input (mozc)"
     end
-    if test $status -eq 0
-        notify-send (string join " " "Switched to" $msg) --icon=keyboard --expire-time=1000
-    else
-        notify-send "Failed to switch ibus engine" --icon=keyboard --expire-time=1000
-        return 1
-    end
+    notify-send (string join " " "Switched to" $msg) --icon=keyboard --expire-time=1000
 end
