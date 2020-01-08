a3ae8b44d5744817adbb70658709de328dba94a9
Jason Nader <jason@kayoway.com>
2020-01-09 00:34:10 +0900

    Run fish_indent on functions

--- fish/.config/fish/functions/wg_whitelist.fish
+++ fish/.config/fish/functions/wg_whitelist.fish
@@ -1,12 +1,12 @@
 function wg_whitelist --description="Script to run before enabling Wireguard to whitelist IPs for Ubuntu sources so can update even when Wireguard service is down"
-#TODO
-     set data (apt-cache policy)
-     for line in $data
-         set --append URLs (string match --regex "^.*(http://\S+)" $line)[2]
-     end
-     for URL in URLs
-         if set --local index (contains --index $URL $URLs)
-   	     set --erase URLs[$index]
-         end
-     end
+    #TODO
+    set data (apt-cache policy)
+    for line in $data
+        set --append URLs (string match --regex "^.*(http://\S+)" $line)[2]
+    end
+    for URL in URLs
+        if set --local index (contains --index $URL $URLs)
+            set --erase URLs[$index]
+        end
+    end
 end
