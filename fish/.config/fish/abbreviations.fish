e1e84263fa31d537988c625a999bce10a6df65de
Jason Nader <jason.nader@protonmail.com>
2020-03-02 22:48:21 +0900

    fish: add abbreviation for cleaning up system

--- fish/.config/fish/abbreviations.fish
+++ fish/.config/fish/abbreviations.fish
@@ -1,6 +1,7 @@
 # Abbreviations (text expansion)
 # After adding a new entry, run `set --erase fish_initialized` and restart fish.
 abbr --add cf '$EDITOR $XDG_CONFIG_HOME/'
+abbr --add cleanup 'paccache --cachedir ~/.cache/pikaur/pkg -vrk0; or paccache -rvk0; and pikaur -Scc'
 abbr --add cp 'cp --interactive --verbose'
 abbr --add gdb 'gdb -nh -x "$XDG_CONFIG_HOME"/gdb/init'
 abbr --add gp 'git push'
