45c7ece13d270838b71fb353bd4c33a6256af68c
Jason Nader <jason.nader@protonmail.com>
2020-03-15 16:44:08 +0900

    fish: add options to pacman-recent function

--- fish/.config/fish/functions/pacman-recent.fish
+++ fish/.config/fish/functions/pacman-recent.fish
@@ -1,3 +1,20 @@
-function pacman-recent --description="Show last 25 upgraded/installed packages"
-    command tac /var/log/pacman.log | head -n 1000 | grep --ignore-case --extended-regexp 'installed|upgraded' | tail --lines=50
+function pacman-recent --description="Show recently upgraded/installed packages"
+    argparse --name pacman-recent 'n/number=' 'p/packages=' -- $argv
+    or return 1
+
+    if set -lq _flag_number
+        set number $_flag_number
+    else
+        set number 25
+    end
+
+    if set -lq _flag_packages
+        set packages $_flag_packages
+    end
+
+    if string length --quiet $packages
+        command tac /var/log/pacman.log | grep --ignore-case --extended-regexp 'installed|upgraded' | grep --extended-regexp (string replace ' ' '|' $packages) | head --lines=$number
+    else
+        command tac /var/log/pacman.log | grep --ignore-case --extended-regexp 'installed|upgraded' | head --lines=$number
+    end
 end
