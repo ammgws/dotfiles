1e695d686684b179649f79b8a5100a8d18c06537
Jason Nader <jason.nader@protonmail.com>
2020-03-16 13:03:59 +0900

    i3status-rust: update AUR update check function
    Re-enable this block when https://github.com/greshake/i3status-rust/pull/515 is merged.

---
+++ fish/.config/fish/functions/check_aur_pending.fish
@@ -0,0 +1,31 @@
+function check_aur_pending --description="Return number of pending AUR updates"
+    argparse --name check_aur_pending h/help b-barmode -- $argv
+    or return 1 # error
+
+    function print_help
+        echo "Usage: check_aur_pending [options]"
+        echo "Options:"
+        echo (set_color green)"--barmode"(set_color $fish_color_normal)": Output text for i3status-rs bar"
+    end
+
+    if set -lq _flag_help
+        print help
+        return
+    end
+
+    set MODE std
+    if set -lq _flag_barmode
+        set MODE bar
+    end
+
+    set val (pikaur --query --sysupgrade --aur 2>/dev/null | wc --lines)
+    if test $MODE = bar
+        if test val = 0
+            printf "{'state': 'Idle', 'text': \"$val\"}"
+        else
+            printf "{'state': 'Warning', 'text': \"$val\"}"
+        end
+    else
+        echo $val
+    end
+end
