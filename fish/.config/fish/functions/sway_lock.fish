15bcfd4d5d09249d950c0fe972f586c2dc4c4984
Jason Nader <jason.nader@protonmail.com>
2020-04-15 22:06:20 +0900

    sway: update lock script

--- fish/.config/fish/functions/sway_lock.fish
+++ fish/.config/fish/functions/sway_lock.fish
@@ -1,5 +1,5 @@
 function sway_lock
-    ratbagctl warbling-mara led 0 set mode off
+    ratbagctl "Logitech G102 Prodigy Gaming Mouse" led 0 set mode off
     swaylock
-    ratbagctl warbling-mara led 0 set mode cycle
+    ratbagctl "Logitech G102 Prodigy Gaming Mouse" led 0 set mode cycle
 end
