d0f0d701c3980bdb1bf3afb7d51d11d477a931cb
Jason Nader <jason.nader@protonmail.com>
2020-04-16 21:07:46 +0900

    fish: abbreviate home dir in fish_title

--- fish/.config/fish/functions/fish_title.fish
+++ fish/.config/fish/functions/fish_title.fish
@@ -1,6 +1,6 @@
 function fish_title
     # emacs is basically the only term that can't handle it.
     if not set -q INSIDE_EMACS
-        echo (status current-command) (__fish_pwd | string replace --regex "^/home/(id --name --user)" "~")
+        echo (status current-command) (__fish_pwd | string replace --regex "^/home/"(id --name --user) "~")
     end
 end
