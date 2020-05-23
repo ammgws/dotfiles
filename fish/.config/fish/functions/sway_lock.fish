36dd0945ac533f2495e283d8a1f9e1aa52a1b9f5
Jason Nader <jason.nader@protonmail.com>
2020-05-23 17:19:55 +0900

    fish/sway_lock: allow args to be passed through

--- fish/.config/fish/functions/sway_lock.fish
+++ fish/.config/fish/functions/sway_lock.fish
@@ -1,5 +1,5 @@
 function sway_lock
     ratbagctl "Logitech G102 Prodigy Gaming Mouse" led 0 set mode off
-    swaylock
+    swaylock $argv
     ratbagctl "Logitech G102 Prodigy Gaming Mouse" led 0 set mode cycle
 end
