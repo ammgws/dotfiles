1abe95bd00a09467e5f17faa0ad809e6efa7844b
Jason Nader <jason.nader@protonmail.com>
2020-05-23 17:07:10 +0900

    fish/fish_title: update

--- fish/.config/fish/functions/fish_title.fish
+++ fish/.config/fish/functions/fish_title.fish
@@ -1,6 +1,12 @@
 function fish_title
     # emacs is basically the only term that can't handle it.
     if not set -q INSIDE_EMACS
-        echo (status current-command) (__fish_pwd | string replace --regex "^/home/"(id --name --user) "~")
+        if string match --quiet --regex micro (status current-command)
+            # TODO: find way to show command args as well so can show filename
+            echo -n (status current-command)
+        else
+            echo -n (status current-command)
+        end
+        echo (__fish_pwd | string replace --regex "^/home/"(id --name --user) " ~")
     end
 end
