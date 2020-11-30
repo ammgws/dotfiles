2a2676b4d8c28261cac78272c74ae5d37821c0a2
Jason Nader <jason.nader@protonmail.com>
2020-11-30 21:40:36 +0900

    fish/i3status-rust: add menu to list X11 clients

--- fish/.config/fish/functions/check_xclients.fish
+++ fish/.config/fish/functions/check_xclients.fish
@@ -1,5 +1,5 @@
 function check_xclients --description="Return number of current X clients"
-    argparse --name check_xclients h/help b-barmode -- $argv
+    argparse --name check_xclients h/help b-barmode m-menu -- $argv
     or return 1 # error
 
     # TODO: get info from swaymsg so can remove this dependency
@@ -12,6 +12,7 @@ function check_xclients --description="Return number of current X clients"
         echo "Usage: check_xclients [options]"
         echo "Options:"
         echo (set_color green)"--barmode"(set_color $fish_color_normal)": Output text for i3status-rs bar"
+        echo (set_color green)"--menu"(set_color $fish_color_normal)": Show popup menu with list of clients"
     end
 
     if set -lq _flag_help
@@ -19,13 +20,8 @@ function check_xclients --description="Return number of current X clients"
         return
     end
 
-    set MODE std
-    if set -lq _flag_barmode
-        set MODE bar
-    end
-
     set val (xlsclients | wc --lines)
-    if test $MODE = bar
+    if set -lq _flag_barmode
         # use hide_when_empty in i3status-rs block config
         if test "$val" = 0
             printf ""
@@ -34,6 +30,8 @@ function check_xclients --description="Return number of current X clients"
         else
             printf "<span color='red'>X11: $val</span>"
         end
+    else if set -lq _flag_menu
+        xlsclients | string sub --start (math (string length -- $hostname) + 3) | wofi --show dmenu
     else
         echo $val
     end
