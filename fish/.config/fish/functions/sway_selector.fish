385bc0f9535d515bd2df1470f37d4ec2f0df5bd5
Jason Nader <jason.nader@protonmail.com>
2020-12-23 09:35:41 +0900

    fish/sway: oops

--- fish/.config/fish/functions/sway_selector.fish
+++ fish/.config/fish/functions/sway_selector.fish
@@ -2,7 +2,7 @@ function sway_selector
     swaymsg --type get_tree | jq -r '.. | select(.pid?) | "\(.pid)\t\(.id)\t\(.name)"' | while read --local pid con_id title
         # TODO handle cases where PID is 0, such as the Steam client
         set --local process_name (cat "/proc/$pid/cmdline")
-        echo -- "$title ($process_name)"
+        echo -- "[$con_id] $title ($process_name)"
     end | wofi --dmenu | read --local selection
     set sel_con (string replace --regex --filter "^\[(\d+)].*" '$1' "$selection")
     swaymsg [con_id="$sel_con"] move container to workspace current
