1d14ceefc86629b32e3b747c2d270c3d5b61872b
Jason Nader <jason.nader@protonmail.com>
2021-04-22 23:02:37 +0900

    fish config: properly guard everything for non-interactive shells

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,10 +11,6 @@ if status is-interactive
     . ~/.config/fish/aliases.fish
 end
 
-set --global __fish_git_prompt_use_informative_chars 1
-set --global __fish_git_prompt_showcolorhints 1
-set --global __fish_git_prompt_char_stateseparator ""
-
 # Universal vars only need to be set once.
 # After adding a new var, run set --erase fish_initialized and restart fish.
 if status is-interactive
@@ -25,92 +21,108 @@ if status is-interactive
     set --universal fish_initialized
 end
 
-# Encourage use of XDG Base Directory spec
-set --export XDG_CACHE_HOME ~/.cache
-set --export XDG_CONFIG_HOME ~/.config
-set --export XDG_DATA_HOME ~/.local/share
-set --export XDG_RUNTIME_DIR /run/user/(id -u)
-set --export CARGO_HOME $XDG_DATA_HOME/cargo
-set --export DVDCSS_CACHE $XDG_DATA_HOME/dvdcss
-set --export GNUPGHOME $XDG_CONFIG_HOME/gnupg
-set --export ICEAUTHORITY $XDG_RUNTIME_DIR/ICEauthority
-set --export LESSHISTFILE $XDG_CACHE_HOME/less/history
-set --export LESSKEY $XDG_CONFIG_HOME/less/lesskey
-set --export MPLAYER_HOME $XDG_CONFIG_HOME/mplayer
-set --export WEECHAT_HOME $XDG_CONFIG_HOME/weechat
-set --export WGETRC $XDG_CONFIG_HOME/wget/wgetrc
-set --export XAUTHORITY $XDG_RUNTIME_DIR/Xauthority
-set --export XINITRC $XDG_CONFIG_HOME/X11/xinitrc
+if status is-login
+    set --global __fish_git_prompt_use_informative_chars 1
+    set --global __fish_git_prompt_showcolorhints 1
+    set --global __fish_git_prompt_char_stateseparator ""
 
-# For Japanese IME support
-set --export GTFW_IM_MODULE ibus
-set --export GTK_IM_MODULE ibus
-set --export QT_IM_MODULE ibus
-set --export XMODIFIERS @im=ibus
-set --export DefaultIMModule ibus
+    # Encourage use of XDG Base Directory spec
+    set --export XDG_CACHE_HOME ~/.cache
+    set --export XDG_CONFIG_HOME ~/.config
+    set --export XDG_DATA_HOME ~/.local/share
+    set --export XDG_RUNTIME_DIR /run/user/(id -u)
+    set --export CARGO_HOME $XDG_DATA_HOME/cargo
+    set --export DVDCSS_CACHE $XDG_DATA_HOME/dvdcss
+    set --export GNUPGHOME $XDG_CONFIG_HOME/gnupg
+    set --export ICEAUTHORITY $XDG_RUNTIME_DIR/ICEauthority
+    set --export LESSHISTFILE $XDG_CACHE_HOME/less/history
+    set --export LESSKEY $XDG_CONFIG_HOME/less/lesskey
+    set --export MPLAYER_HOME $XDG_CONFIG_HOME/mplayer
+    set --export WEECHAT_HOME $XDG_CONFIG_HOME/weechat
+    set --export WGETRC $XDG_CONFIG_HOME/wget/wgetrc
+    set --export XAUTHORITY $XDG_RUNTIME_DIR/Xauthority
+    set --export XINITRC $XDG_CONFIG_HOME/X11/xinitrc
 
-# Other
-set --export BROWSER /usr/bin/firefox-nightly
-set --export EDITOR micro
-set --export FZF_DEFAULT_COMMAND "fd --type f"
-set --export GIT_EDITOR micro
-set --export MANPAGER "fish --command 'col --no-backspaces --spaces | bat --language man --plain'" # use bat to colourise man
-set --export MOZ_DBUS_REMOTE 1 # allows X11 and Wayland Firefox instances to run together
-set --export MOZ_WEBRENDER 1
-set --export SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS 0 # for when playing games
-set --export SHELL /usr/bin/fish
-set --export TERMINAL kitty
-set --export VDPAU_DRIVER radeonsi # keeps trying to use nvidia driver
-set --export XKB_DEFAULT_LAYOUT us
+    # For Japanese IME support
+    set --export GTFW_IM_MODULE ibus
+    set --export GTK_IM_MODULE ibus
+    set --export QT_IM_MODULE ibus
+    set --export XMODIFIERS @im=ibus
+    set --export DefaultIMModule ibus
 
-# Used in my fish functions
-set --export SCREENSHOT_DIR $HOME/Dropbox/screenshots
+    # Other
+    set --export BROWSER /usr/bin/firefox-nightly
+    set --export EDITOR micro
+    set --export FZF_DEFAULT_COMMAND "fd --type f"
+    set --export GIT_EDITOR micro
+    set --export MANPAGER "fish --command 'col --no-backspaces --spaces | bat --language man --plain'" # use bat to colourise man
+    set --export MOZ_DBUS_REMOTE 1 # allows X11 and Wayland Firefox instances to run together
+    set --export MOZ_WEBRENDER 1
+    set --export SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS 0 # for when playing games
+    set --export SHELL /usr/bin/fish
+    set --export TERMINAL kitty
+    set --export VDPAU_DRIVER radeonsi # keeps trying to use nvidia driver
+    set --export XKB_DEFAULT_LAYOUT us
 
-# # gpg-agent manpage: always add this to whatever init file is used for all shell invocations
-set --export GPG_TTY (tty)
-
-# Make ssh use gpg-agent instad of ssh-agent
-set --erase SSH_AGENT_PID
-if not set --query gnupg_SSH_AUTH_SOCK_by
-    or test $gnupg_SSH_AUTH_SOCK_by -ne $fish_pid
-    set --export SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    # Used in my fish functions
+    set --export SCREENSHOT_DIR $HOME/Dropbox/screenshots
 end
-# gpg-agent manpage: Since the ssh-agent protocol does not contain a mechanism for telling the agent on which
-# display/terminal it is running, gpg-agent’s ssh-support will use the TTY or X display where gpg-agent has
-# been started. To switch this display to the current one, the following command may be used:
-gpg-connect-agent updatestartuptty /bye >/dev/null
 
-# User experience improvements over SSH
-if set --query SSH_CLIENT
-    set --erase BROWSER
+if status is-interactive
+    # gpg-agent manpage: always add this to whatever init file is used for all shell invocations
+    set --export GPG_TTY (tty)
 
-    if ! set --query XDG_RUNTIME_DIR
-        set --export XDG_RUNTIME_DIR /run/user/(id -u)
+    # Make ssh use gpg-agent instad of ssh-agent
+    if status is-login
+        set --erase SSH_AGENT_PID
+        if not set --query gnupg_SSH_AUTH_SOCK_by
+            or test $gnupg_SSH_AUTH_SOCK_by -ne $fish_pid
+            set --export SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+        end
+        #dbus-update-activation-environment --systemd SSH_AUTH_SOCK
     end
 
-    if ! set --query DBUS_SESSION_BUS_ADDRESS -o test -z $DBUS_SESSION_ADDRESS
-        set --local dbus_session_file $HOME/.dbus/session-bus/(cat /var/lib/dbus/machine-id)-0
-        if test -e $dbus_session_file
-            while read --local --array line
-                set match (string match --regex "^DBUS_SESSION_BUS_ADDRESS=(.+)" $line)
-                if test $status -eq 0
-                    set --export DBUS_SESSION_BUS_ADDRESS $match[2]
-                end
-            end <$dbus_session_file
+    # gpg-agent manpage: Since the ssh-agent protocol does not contain a mechanism for telling the agent on which
+    # display/terminal it is running, gpg-agent’s ssh-support will use the TTY or X display where gpg-agent has
+    # been started. To switch this display to the current one, the following command may be used:
+    gpg-connect-agent updatestartuptty /bye >/dev/null
+end
+
+if status is-interactive
+    # User experience improvements over SSH
+    if set --query SSH_CLIENT
+        set --erase BROWSER
+
+        if ! set --query XDG_RUNTIME_DIR
+            set --export XDG_RUNTIME_DIR /run/user/(id -u)
+        end
+
+        if ! set --query DBUS_SESSION_BUS_ADDRESS -o test -z $DBUS_SESSION_ADDRESS
+            set --local dbus_session_file $HOME/.dbus/session-bus/(cat /var/lib/dbus/machine-id)-0
+            if test -e $dbus_session_file
+                while read --local --array line
+                    set match (string match --regex "^DBUS_SESSION_BUS_ADDRESS=(.+)" $line)
+                    if test $status -eq 0
+                        set --export DBUS_SESSION_BUS_ADDRESS $match[2]
+                    end
+                end <$dbus_session_file
+            end
+        end
+
+        if ! set --query SWAYSOCK
+            and pidof sway
+            set --export SWAYSOCK /run/user/(id -u)/sway-ipc.(id -u).(pidof sway).sock
+        end
+
+        # Change editor when remoting in from phone
+        set ip (string match --regex "(\d+.\d+.\d+.\d)" $SSH_CONNECTION)[2]
+        if test "$ip" = "10.8.7.2"
+            set --export GIT_EDITOR micro
+            set --export EDITOR micro
         end
     end
-
-    if ! set --query SWAYSOCK
-        and pidof sway
-        set --export SWAYSOCK /run/user/(id -u)/sway-ipc.(id -u).(pidof sway).sock
-    end
-
-    # Change editor when remoting in from phone
-    set ip (string match --regex "(\d+.\d+.\d+.\d)" $SSH_CONNECTION)[2]
-    if test "$ip" = "10.8.7.2"
-        set --export GIT_EDITOR micro
-        set --export EDITOR micro
-    end
 end
 
-thefuck --alias | source
+if status is-interactive
+    thefuck --alias | source
+end
