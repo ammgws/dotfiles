2dafcd5a5c778209b91ae18b0478f52a09dd0254
Jason Nader <jason.nader@protonmail.com>
2021-06-20 13:14:53 +0900

    fish: run fish_indent on functions

--- fish/.config/fish/functions/sway_idle.fish
+++ fish/.config/fish/functions/sway_idle.fish
@@ -1,11 +1,10 @@
 function sway_idle
-# To make sure swayidle waits for swaylock to lock the screen before it releases the inhibition lock,
-# swayidle's -w (wait) option and swaylock's -f/--daemonize options are used.
-swayidle -d -w \
-     timeout 300 'fish --command="sway_lock --debug 2>>/tmp/swaylock_timeout300.log"' \
-     idlehint 300 \
-     timeout 600 'swaymsg "output * dpms off"' \
-          resume 'swaymsg "output * dpms on"' \
-     before-sleep 'fish --command="sway_lock --debug 2>>/tmp/swaylock_beforesleep.log"' \
-     2>>/tmp/swayidle.log
+    # To make sure swayidle waits for swaylock to lock the screen before it releases the inhibition lock,
+    # swayidle's -w (wait) option and swaylock's -f/--daemonize options are used.
+    swayidle -d -w \
+        timeout 300 'fish --command="sway_lock --debug 2>>/tmp/swaylock_timeout300.log"' \
+        idlehint 300 \
+        timeout 600 'swaymsg "output * dpms off"' \
+        resume 'swaymsg "output * dpms on"' \
+        before-sleep 'fish --command="sway_lock --debug 2>>/tmp/swaylock_beforesleep.log"' 2>>/tmp/swayidle.log
 end
