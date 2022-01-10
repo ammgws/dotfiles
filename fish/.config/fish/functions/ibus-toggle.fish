0a486ddf307bdeee5b2f9975d38adbef4f4972a8
Jason Nader <jason.nader@protonmail.com>
2022-01-10 16:44:12 +0900

    ibus toggle script: last used updateS

--- fish/.config/fish/functions/ibus-toggle.fish
+++ fish/.config/fish/functions/ibus-toggle.fish
@@ -1,21 +1,14 @@
 function ibus-toggle --description="Switches between IBus input methods and displays notification."
-    # On first startup the first call always seems to fail...
-    set engine (ibus engine 2>/dev/null)
+    # On first startup the first call fails with 'no global engine',
+    # so just set it to EN.
     set engine (ibus engine 2>/dev/null)
     if test $status -ne 0
-        set msg "Failed to switch ibus engine"
-        echo $msg
-        notify-send "$msg" --icon=keyboard --expire-time=1000
-        return 1
+        ibus engine xkb:us::eng
+        return 0
     end
     if test "$engine" = mozc-jp
         ibus engine xkb:us::eng
-        set new_engine "English input"
     else
         ibus engine mozc-jp
-        set new_engine "Japanese input (mozc)"
     end
-    set msg "Switched to $new_engine"
-    echo $msg
-    notify-send "$msg" --icon=keyboard --expire-time=1000
 end
