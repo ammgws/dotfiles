9c08855cd67aac647c517477b4284374dcd1654e
Jason Nader <jason.nader@protonmail.com>
2022-02-06 23:38:47 +0900

    fish: run fish_indent on functions

--- fish/.config/fish/functions/bump-pkgbuild.fish
+++ fish/.config/fish/functions/bump-pkgbuild.fish
@@ -1,8 +1,8 @@
 function bump-pkgbuild --description 'Update PKGBUILd to new version' --arguments new_version
-	sed -E "s#(pkgver=).*#\1$new_version#" -i PKGBUILD
+    sed -E "s#(pkgver=).*#\1$new_version#" -i PKGBUILD
 
-	updpkgsums
-	makepkg --printsrcinfo > .SRCINFO
-	
-	git commit -v -a -m "Update to $new_version"
+    updpkgsums
+    makepkg --printsrcinfo >.SRCINFO
+
+    git commit -v -a -m "Update to $new_version"
 end
--- fish/.config/fish/functions/sway_setup_tv_gaming.fish
+++ fish/.config/fish/functions/sway_setup_tv_gaming.fish
@@ -1,6 +1,7 @@
 function sway_setup_tv_gaming
     notify-send "Starting gaming mode!"
-    lgtv wakeonlan and; lgtv tv switchInput HDMI_1
+    lgtv wakeonlan and
+    lgtv tv switchInput HDMI_1
 
     set tv "'Goldstar Company Ltd LG TV 0x00000101'"
     swaymsg output "$tv" enable
@@ -9,12 +10,13 @@ function sway_setup_tv_gaming
     set tv_outputname (swaymsg -t get_outputs | jq --raw-output '.. | objects | select(.model == "LG TV") | .name')
     set tv_workspace (swaymsg -t get_outputs | jq --raw-output '.. | objects | select(.model == "LG TV") | .current_workspace')
 
-    swaymsg exec -- steam-native -start steam://open/bigpicture > /dev/null 2>&1 &
+    swaymsg exec -- steam-native -start steam://open/bigpicture >/dev/null 2>&1 &
     sleep 5
     # big picture mode class is lowercase, normal steam client is capitalised
     # don't know if this is intentional, but is handy for us!
     swaymsg [class=steam] move container to workspace "$tv_workspace"
     swaymsg focus output "$tv_outputname"
     swaymsg [class=steam] fullscreen
-    switchaudio --device TV and; pamixer --set-volume 100
+    switchaudio --device TV and
+    pamixer --set-volume 100
 end
