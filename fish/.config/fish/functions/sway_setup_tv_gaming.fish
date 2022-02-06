437a99b71ce5a132506b6141632a8469ee24d992
Jason Nader <jason.nader@protonmail.com>
2022-02-06 23:37:12 +0900

    fish: update TV gaming scripts

--- fish/.config/fish/functions/sway_setup_tv_gaming.fish
+++ fish/.config/fish/functions/sway_setup_tv_gaming.fish
@@ -1,17 +1,20 @@
 function sway_setup_tv_gaming
-    lgtv wakeonlan
-    lgtv tv switchInput HDMI_1
+    notify-send "Starting gaming mode!"
+    lgtv wakeonlan and; lgtv tv switchInput HDMI_1
 
     set tv "'Goldstar Company Ltd LG TV 0x00000101'"
-
     swaymsg output "$tv" enable
+    swaymsg output "$tv" dpms on
+
     set tv_outputname (swaymsg -t get_outputs | jq --raw-output '.. | objects | select(.model == "LG TV") | .name')
     set tv_workspace (swaymsg -t get_outputs | jq --raw-output '.. | objects | select(.model == "LG TV") | .current_workspace')
 
-    steam-native -start steam://open/bigpicture &
+    swaymsg exec -- steam-native -start steam://open/bigpicture > /dev/null 2>&1 &
+    sleep 5
     # big picture mode class is lowercase, normal steam client is capitalised
     # don't know if this is intentional, but is handy for us!
     swaymsg [class=steam] move container to workspace "$tv_workspace"
     swaymsg focus output "$tv_outputname"
+    swaymsg [class=steam] fullscreen
     switchaudio --device TV and; pamixer --set-volume 100
 end
