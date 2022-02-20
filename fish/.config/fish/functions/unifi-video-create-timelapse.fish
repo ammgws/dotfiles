c51ae6db513606f0456c75d1984d7fd1fc0beb76
Jason Nader <jason.nader@protonmail.com>
2022-02-20 20:09:19 +0900

    fish/unifi-video script: use tmp file for file list

--- fish/.config/fish/functions/unifi-video-create-timelapse.fish
+++ fish/.config/fish/functions/unifi-video-create-timelapse.fish
@@ -7,9 +7,10 @@ function unifi-video-create-timelapse --argument-names unifi_videos_dir output_t
     string length --quiet "$output_text"
     or return 1
 
+    set listfile (mktemp --suffix ".txt")
     for f in $unifi_videos_dir/*.mp4
-        echo "file '$f'" >>$unifi_videos_dir/list.txt
+        echo "file '$f'" >>$listfile
     end
-    ffmpeg -f concat -safe 0 -i $unifi_videos_dir/list.txt -c copy $unifi_videos_dir/sleep.mp4
-    ffmpeg -i $unifi_videos_dir/$output_text.mp4 -filter:v "setpts=0.05*PTS" -an $unifi_videos_dir/$output_timelapse.mp4
+    ffmpeg -f concat -safe 0 -i $listfile -c copy $unifi_videos_dir/$output_text.mp4
+    ffmpeg -i "$unifi_videos_dir/$output_text.mp4" -filter:v "setpts=0.05*PTS" -an "$unifi_videos_dir/$output_text_timelapse.mp4"
 end
