9b9f318c2a1ef1f9402b12d988b1eada4af6ab8d
Jason Nader <jason.nader@protonmail.com>
2022-02-27 17:35:35 +0900

    sway/fish: update sway_lock command

--- fish/.config/fish/functions/sway_lock.fish
+++ fish/.config/fish/functions/sway_lock.fish
@@ -1,5 +1,7 @@
 function sway_lock
     ratbagctl "Logitech G102 Prodigy Gaming Mouse" led 0 set mode off
+    swaymsg --raw output '*' dpms off
     swaylock $argv
     ratbagctl "Logitech G102 Prodigy Gaming Mouse" led 0 set mode cycle
+    swaymsg --raw output '*' dpms on
 end
