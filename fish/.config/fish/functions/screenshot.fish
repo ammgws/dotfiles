b11ffe19fce39c222ceb6a983c6084b26d6bdff2
Jason Nader <jason.nader@protonmail.com>
2022-03-04 12:29:22 +0900

    fish/screenshot: remove dropbox upload support

--- fish/.config/fish/functions/screenshot.fish
+++ fish/.config/fish/functions/screenshot.fish
@@ -1,7 +1,5 @@
-function screenshot --description="When using `sway`: Takes screenshot, uploads to Dropbox and copies link to clipboard."
+function screenshot --description="Saves screenshot and also copies image to clipboard."
     set SCREENSHOT_DIR $SCREENSHOT_DIR
-    set WAIT_TIME 5
-    set TIMEOUT 3
 
     # define colours to use when printing messages
     set cRed (set_color red)
@@ -12,8 +10,7 @@ function screenshot --description="When using `sway`: Takes screenshot, uploads 
     function print_help
         echo "Usage: screenshot [options]"
         echo "Options:"
-        echo (set_color green)"-l"(set_color $fish_color_normal)": Only output URL to clipboard (default outputs image to clipboard and URL to primary)"
-        echo (set_color green)"-o"(set_color $fish_color_normal)": Open URL in browser after upload"
+        echo (set_color green)"-h"(set_color $fish_color_normal)": Prints this help"
     end
 
     # default values (that can be changed via args)
@@ -22,7 +19,7 @@ function screenshot --description="When using `sway`: Takes screenshot, uploads 
     set WM sway
     set DEPENDENCIES grim slurp wl-copy
 
-    argparse --name screenshot h/help i-i3 l/linkonly o/openafter -- $argv
+    argparse --name screenshot h/help i-i3 -- $argv
     or return 1 #error
 
     if set -lq _flag_help
@@ -35,15 +32,6 @@ function screenshot --description="When using `sway`: Takes screenshot, uploads 
         set WM i3
     end
 
-    if set -lq _flag_linkonly
-        set OUTPUT_MODE linkonly
-    end
-
-    if set -lq _flag_openafter
-        set OPEN_URL 1
-    end
-
-    set DEPENDENCIES $DEPENDENCIES dropbox-cli
     for dependency in $DEPENDENCIES
         if not type -q $dependency
             echo $cRed"You must have $dependency installed."$cRst
@@ -65,45 +53,11 @@ function screenshot --description="When using `sway`: Takes screenshot, uploads 
         return 1
     end
 
-    function getstatus
-        set SYNC_STATUS (dropbox-cli filestatus $FILENAME)
-
-        if test (string match $SYNC_STATUS = 'up to date')
-            return 0
-        else
-            return 1
-        end
+    if test $WM = i3
+        xclip -selection clip -target image/png $FILENAME
+        echo -n $DROPBOX_LINK | xclip -selection primary
+    else
+        wl-copy --type image/png <$FILENAME
     end
-
-    getstatus
-    set TIME_ELAPSED 0
-    while test $status -eq 1; and test $TIME_ELAPSED -lt $TIMEOUT
-        sleep 0.5
-        set TIME_ELAPSED (math $TIME_ELAPSED + 1)
-    end
-
-    set DROPBOX_LINK (string join "" (dropbox-cli sharelink $FILENAME) "&raw=1")
-
-    if test $OUTPUT_MODE = linkonly
-        if test $WM = i3
-            echo -n $DROPBOX_LINK | xclip -selection clip
-        else
-            echo -n $DROPBOX_LINK | wl-copy
-        end
-        notify-send Screenshot $DROPBOX_LINK --icon=$FILENAME --expire-time=2000
-    else if test $OUTPUT_MODE = image
-        if test $WM = i3
-            xclip -selection clip -target image/png $FILENAME
-            echo -n $DROPBOX_LINK | xclip -selection primary
-        else
-            wl-copy --type image/png <$FILENAME
-        end
-        notify-send Screenshot $FILENAME --icon=$FILENAME --expire-time=2000
-    end
-
-    if test $OPEN_URL = 1
-        open $DROPBOX_LINK
-    end
-
-    sleep $WAIT_TIME
+    notify-send Screenshot $FILENAME --icon=$FILENAME --expire-time=2000
 end
