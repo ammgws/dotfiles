6dad7d04b671f5984a55edd69a4091a1626fd33e
Jason Nader <jason.nader@protonmail.com>
2022-03-18 18:27:18 +0900

    fish greeting: switch to prompt_hostname

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -2,7 +2,7 @@ function fish_greeting
     if set --query SSH_CLIENT
         or status is-login
         and not set --query TMUX
-        set msg (string join "\n" "Welcome "(whoami)@(hostname)"." \
+        set msg (string join "\n" "Welcome "(whoami)@(prompt_hostname)"." \
       (uname --machine --kernel-release --kernel-name) \
       (uptime --pretty) \
       (systemd-analyze time)
