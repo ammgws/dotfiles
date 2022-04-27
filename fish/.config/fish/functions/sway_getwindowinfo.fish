6256976e5c444ef2d9292839719adbd48face080
Jason Nader <jason.nader@protonmail.com>
2022-04-27 19:24:28 +0900

    fish/sway: update helper function

--- fish/.config/fish/functions/sway_getwindowinfo.fish
+++ fish/.config/fish/functions/sway_getwindowinfo.fish
@@ -4,21 +4,25 @@ function sway_getwindowinfo
 
     if set -lq _flag_focused_window
         swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible? and .focused?)'
-    else
-        slurp -p -f "%x %y" | read x_sel y_sel
-        or return 1
+        return 0
+    end
 
-        # TODO: this returns everything when there is only one container open in the workspace
-        # need to fix the logic
-        for rect in (swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x) \(.width) \(.y) \(.height)"')
-            echo $rect | read x1 w y1 h
-            set x2 (math $x1 + $w)
-            set y2 (math $y1 + $h)
-            if test \( $x_sel -ge $x1 \) -a \( $x_sel -le $x2 \)
-                and test \( $y_sel -ge $y1 \) -a \( $y_sel -le $y2 \)
-                #swaymsg --type get_tree | jq ".. | objects | select(.rect.x == $x1 and .rect.y == $y1 and .rect.width == $w and .rect.height == $h) | .id, .name, .pid, .title, .app_id, .marks, .type"
-                swaymsg --type get_tree | jq ".. | objects | select(.rect.x == $x1 and .rect.y == $y1 and .rect.width == $w and .rect.height == $h)"
-            end
+    if test (count $argv) -eq 3
+        set x_sel $argv[1]
+        set y_sel $argv[2]
+        set output_name $argv[3]
+    else
+        slurp -p -f "%x %y %o" | read x_sel y_sel output_name
+        or return 1
+    end
+
+    for rect in (swaymsg -t get_tree | jq -r ".. | objects | select(.name == \"$output_name\") | recurse | select(.pid? and .visible?) | .rect | \"\(.x) \(.width) \(.y) \(.height)\"")
+        echo $rect | read x1 w y1 h
+        set x2 (math $x1 + $w)
+        set y2 (math $y1 + $h)
+        if test \( $x_sel -ge $x1 \) -a \( $x_sel -le $x2 \)
+            and test \( $y_sel -ge $y1 \) -a \( $y_sel -le $y2 \)
+            swaymsg --type get_tree | jq ".. | objects | select(.name == \"$output_name\") | recurse | select(.pid? and .visible?) | select(.rect.x == $x1 and .rect.y == $y1 and .rect.width == $w and .rect.height == $h)"
         end
     end
 end
