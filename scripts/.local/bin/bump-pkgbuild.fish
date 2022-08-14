8e05872f1f7872434bf2fd6b6690b60b8ff8e0d5
Jason Nader <jason.nader@protonmail.com>
2022-08-14 12:41:28 +0900

    Rename script

--- scripts/.local/bin/bump-pkgbuild.fish
+++
@@ -1,8 +0,0 @@
-function bump-pkgbuild --description 'Update PKGBUILd to new version' --arguments new_version
-    sed -E "s#(pkgver=).*#\1$new_version#" -i PKGBUILD
-
-    updpkgsums
-    makepkg --printsrcinfo >.SRCINFO
-
-    git commit -v -a -m "Update to $new_version"
-end
